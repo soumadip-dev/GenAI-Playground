fd9b5e43724b23632138786ff9e8618e6b27fe5c
soumadip-dev <soumadipmajila@gmail.com>
2025-11-28 23:14:53 +0530

    feat: Add Jinja2 templating, static file serving, and MongoDB integration, removing Pydantic test code.

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from pymongo import MongoClient
+
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+templates = Jinja2Templates(directory="templates")
+
+conn = MongoClient(
+    "mongodb+srv://soumadipmajila:8Uh9M96cZq@cluster0.fn2t7ng.mongodb.net"
+)
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/", response_class=HTMLResponse)
+async def read_item(request: Request):
+    docs = conn.fastapi.notes.find({})
+    for doc in docs:
+        print(doc)
+    # print(docs)
+    return templates.TemplateResponse("index.html", {"request": request})
