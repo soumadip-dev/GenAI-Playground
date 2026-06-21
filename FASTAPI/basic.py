bdbca3df01ede48ca8d4d2ecd6ef10c66d098125
soumadip-dev <soumadipmajila@gmail.com>
2026-06-21 14:42:20 +0530

    feat: learn FastAPI query parameters with dynamic routes

--- FASTAPI/basic.py
+++ FASTAPI/basic.py
@@ -7,10 +7,7 @@ app = FastAPI()
 def home():
     return {"message": "My first FastAPI server is running ✅"}
 
+
 @app.get("/about")
 def about():
     return {"project": "loan risk model", "version": "0.1"}
-
-@app.get("/customer")
-def get_customer(customer_id: int):
-    return {"customer_id": customer_id, "name": "John Doe"}
